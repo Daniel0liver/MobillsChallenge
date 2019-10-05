c46cc73434d731b35e81d1c04f274315383df0da
Daniel Oliveira <dan.oliveira461@gmail.com>
2019-10-05 09:37:50 -0300

    Create component to edit expenses

---
+++ mobills/src/components/Expense/EditExpense.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import {StyleSheet, View, TouchableOpacity, Text} from 'react-native';
+
+class EditExpense extends Component {
+  static navigationOptions = {
+    title: 'Editar Despesa',
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Editar</Text>
+        <TouchableOpacity
+          style={styles.buttonLeft}
+          onPress={() => this.props.navigation.push('EditExpenseRoute')}>
+          <Text>Detalhes</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.buttonLeft}
+          onPress={() => this.props.navigation.navigate('ExpenseRoute')}>
+          <Text>Inicio</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.buttonLeft}
+          onPress={() => this.props.navigation.goBack()}>
+          <Text>Voltar</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonLeft: {
+    alignSelf: 'stretch',
+    padding: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+    backgroundColor: '#2173d2',
+    marginBottom: 10,
+  },
+  textButton: {
+    fontSize: 25,
+    color: '#fff',
+  },
+});
+
+export default EditExpense;
